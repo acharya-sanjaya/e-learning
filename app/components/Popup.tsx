7296eddaedcc1fbb6951f844161c98427a145ce2
Sanjaya Acharya <92267442+acharya-sanjaya@users.noreply.github.com>
2024-11-07 17:46:45 +0545

    create popup container (#8)

---
+++ app/components/Popup.tsx
@@ -0,0 +1,62 @@
+import {ReactNode, useEffect, useState} from "react";
+import Icon from "~/Icons";
+
+const Popup = ({onClose, children}: {onClose: () => void; children: ReactNode}) => {
+  const [applyInitialValue, setApplyInitialValue] = useState(true);
+  const [popupTranslation, setPopupTranslation] = useState(
+    "scale(1) translateY(calc(-50svh - 50%))"
+  );
+  const transitionTime = 300;
+
+  useEffect(() => {
+    // This is to force transition effect on load
+    const timeout = setTimeout(() => {
+      setApplyInitialValue(false);
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const closePopup = () => {
+    setPopupTranslation("scale(0) translate(0)");
+
+    setTimeout(() => {
+      onClose && onClose();
+    }, transitionTime);
+  };
+
+  return (
+    <div
+      role="button"
+      tabIndex={-1}
+      onClick={closePopup}
+      onKeyDown={(e) => e.key === "Enter" && closePopup()}
+      className="fixed pt-[100svh] bg-black/40 backdrop-blur-md w-svw h-svh top-0 left-0 flex justify-center"
+      style={{
+        zIndex: 9999,
+      }}
+    >
+      <div
+        role="none"
+        onClick={(event) => event.stopPropagation()}
+        className="cursor-default relative border-2 border-gray-600 rounded-lg p-10 flex justify-center items-center overflow-hidden bg-white/20"
+        style={{
+          width: "90%",
+          height: "fit-content",
+          maxHeight: "100svh",
+          transform: applyInitialValue ? "scale(0) translateY(0)" : popupTranslation,
+          transition: `transform ${transitionTime}ms ease-in-out`,
+        }}
+      >
+        <Icon
+          iconName="close"
+          thickness={2}
+          className="cursor-pointer size-10 p-2 absolute top-0.5 right-0.5 stroke-white active:stroke-red-500"
+          onClick={closePopup}
+        />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;

6b760a87fb69264cc86f18ce4dcb26c46855bfb5
Sanjaya Acharya <92267442+acharya-sanjaya@users.noreply.github.com>
2024-11-16 14:19:33 +0545

    add data for nihongo verbs (#15)

---
+++ app/data/japanese/verbs-part1.ts
@@ -0,0 +1,254 @@
+const questions = [
+  {
+    question: "to meet, to see",
+    answer: "au",
+    display: "あう",
+  },
+  {
+    question: "to play",
+    answer: "asobu",
+    display: "あそぶ",
+  },
+  {
+    question: "to wash",
+    answer: "arau",
+    display: "あらう",
+  },
+  {
+    question: "to be (object)",
+    answer: "aru",
+    display: "ある",
+  },
+  {
+    question: "to say",
+    answer: "iu",
+    display: "いう",
+  },
+  {
+    question: "to go",
+    answer: "iku",
+    display: "いく",
+  },
+  {
+    question: "to be (living beings)",
+    answer: "iru",
+    display: "いる",
+  },
+  {
+    question: "to sing",
+    answer: "utau",
+    display: "うたう",
+  },
+  {
+    question: "to sell",
+    answer: "uru",
+    display: "うる",
+  },
+  {
+    question: "to wake up",
+    answer: "okiru",
+    display: "おきる",
+  },
+  {
+    question: "to think",
+    answer: "omou",
+    display: "おもう",
+  },
+  {
+    question: "to buy",
+    answer: "kau",
+    display: "かう",
+  },
+  {
+    question: "to return",
+    answer: "kaeru",
+    display: "かえる",
+  },
+  {
+    question: "to write",
+    answer: "kaku",
+    display: "かく",
+  },
+  {
+    question: "to listen",
+    answer: "kiku",
+    display: "きく",
+  },
+  {
+    question: "to come",
+    answer: "kuru",
+    display: "くる",
+  },
+  {
+    question: "to know",
+    answer: "shiru",
+    display: "しる",
+  },
+  {
+    question: "to do, to play",
+    answer: "suru",
+    display: "する",
+  },
+  {
+    question: "to eat",
+    answer: "taberu",
+    display: "たべる",
+  },
+  {
+    question: "to sleep",
+    answer: "neru",
+    display: "ねる",
+  },
+  {
+    question: "to go out, to leave",
+    answer: "deru",
+    display: "でる",
+  },
+  {
+    question: "to drink",
+    answer: "nomu",
+    display: "のむ",
+  },
+  {
+    question: "to enter",
+    answer: "hairu",
+    display: "はいる",
+  },
+  {
+    question: "to work",
+    answer: "hataraku",
+    display: "はたらく",
+  },
+  {
+    question: "to speak",
+    answer: "hanasu",
+    display: "はなす",
+  },
+  {
+    question: "to watch",
+    answer: "miru",
+    display: "みる",
+  },
+  {
+    question: "to rest",
+    answer: "yasumu",
+    display: "やすむ",
+  },
+  {
+    question: "to read",
+    answer: "yomu",
+    display: "よむ",
+  },
+  {
+    question: "to understand",
+    answer: "wakaru",
+    display: "わかる",
+  },
+  {
+    question: "to make",
+    answer: "tsukuru",
+    display: "つくる",
+  },
+  {
+    question: "to wait",
+    answer: "matsu",
+    display: "まつ",
+  },
+  {
+    question: "to teach",
+    answer: "oshieru",
+    display: "おしえる",
+  },
+  {
+    question: "to live, to reside",
+    answer: "sumu",
+    display: "すむ",
+  },
+  {
+    question: "to use",
+    answer: "tsukau",
+    display: "つかう",
+  },
+  {
+    question: "to have, to hold",
+    answer: "motsu",
+    display: "もつ",
+  },
+  {
+    question: "to open (something)",
+    answer: "akeru",
+    display: "あける",
+  },
+  {
+    question: "to give",
+    answer: "ageru",
+    display: "あげる",
+  },
+  {
+    question: "to put inside",
+    answer: "ireru",
+    display: "いれる",
+  },
+  {
+    question: "to born",
+    answer: "umareru",
+    display: "うまれる",
+  },
+  {
+    question: "to put, to place",
+    answer: "oku",
+    display: "おく",
+  },
+  {
+    question: "to send",
+    answer: "okuru",
+    display: "おくる",
+  },
+  {
+    question: "to memorize",
+    answer: "oboeru",
+    display: "おぼえる",
+  },
+  {
+    question: "to swim",
+    answer: "oyogu",
+    display: "およぐ",
+  },
+  {
+    question: "to get off, to get down",
+    answer: "oriru",
+    display: "おりる",
+  },
+  {
+    question: "to lend",
+    answer: "kasu",
+    display: "かす",
+  },
+  {
+    question: "to borrow",
+    answer: "kariru",
+    display: "かりる",
+  },
+  {
+    question: "to think, to consider",
+    answer: "kangaeru",
+    display: "かんがえる",
+  },
+  {
+    question: "to cut",
+    answer: "kiru",
+    display: "きる",
+  },
+  {
+    question: "to wear",
+    answer: "kiru",
+    display: "きる",
+  },
+  {
+    question: "to put out, to turn off",
+    answer: "kesu",
+    display: "けす",
+  },
+];
+
+export default questions;
---
+++ app/data/japanese/verbs-part3.ts
@@ -0,0 +1,133 @@
+const questions = [
+  {
+    question: "to bring (something)",
+    answer: "motsutekuru",
+    display: "もつてくる",
+  },
+  {
+    question: "to give up, to quit",
+    answer: "yameru",
+    display: "やめる",
+  },
+  {
+    question: "to draw, to paint",
+    answer: "kaku",
+    display: "かく",
+  },
+  {
+    question: "to put, to set",
+    answer: "tsukeru",
+    display: "つける",
+  },
+  {
+    question: "to reserve",
+    answer: "yoyakusuru",
+    display: "よやくする",
+  },
+  {
+    question: "to practice",
+    answer: "renshuusuru",
+    display: "れんしゅうする",
+  },
+  {
+    question: "to get together, to collect",
+    answer: "atsumeru",
+    display: "あつめる",
+  },
+  {
+    question: "to fix, to repair",
+    answer: "shuurisuru",
+    display: "しゅうりする",
+  },
+  {
+    question: "to present",
+    answer: "shoukaisuru",
+    display: "しょうかいする",
+  },
+  {
+    question: "to move",
+    answer: "ugoku",
+    display: "うごく",
+  },
+  {
+    question: "to take down, to put down",
+    answer: "orosu",
+    display: "おろす",
+  },
+  {
+    question: "to hang, to suspend",
+    answer: "kakeru",
+    display: "かける",
+  },
+  {
+    question: "to put (helmet, hat) on",
+    answer: "kaburu",
+    display: "かぶる",
+  },
+  {
+    question: "to ask",
+    answer: "kiku",
+    display: "きく",
+  },
+  {
+    question: "to touch",
+    answer: "sawaru",
+    display: "さわる",
+  },
+  {
+    question: "to search, to examine",
+    answer: "shiraberu",
+    display: "しらべる",
+  },
+  {
+    question: "to suit, to be convenient",
+    answer: "tsugougaii",
+    display: "つごうがいい",
+  },
+  {
+    question: "to be inconvenient, not to suit",
+    answer: "tsugougawarui",
+    display: "つごうがわるい",
+  },
+  {
+    question: "to stay, to put up",
+    answer: "tomaru",
+    display: "とまる",
+  },
+  {
+    question: "to correct, to fix",
+    answer: "naosu",
+    display: "なおす",
+  },
+  {
+    question: "to change (train, bus)",
+    answer: "norikaeru",
+    display: "のりかえる",
+  },
+  {
+    question: "to turn (something)",
+    answer: "mawasu",
+    display: "まわす",
+  },
+  {
+    question: "to polish",
+    answer: "migaku",
+    display: "みがく",
+  },
+  {
+    question: "to welcome, to receive",
+    answer: "mukaeru",
+    display: "むかえる",
+  },
+  {
+    question: "to be useful",
+    answer: "yakunitatsu",
+    display: "やくにたつ",
+  },
+  {
+    question: "to cross, to go accross",
+    answer: "wataru",
+    display: "わたる",
+  },
+];
+export default questions;

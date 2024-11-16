7beb30dd7f67fb30549b49d2d2ee1b9c9608717e
acharya-sanjaya <amitacharya22780@gmail.com>
2024-11-16 13:40:31 +0545

    add data for nihongo verbs

---
+++ app/data/japanese/verbs-part2.ts
@@ -0,0 +1,259 @@
+const questions = [
+  {
+    question: "to take walk, to stroll",
+    answer: "sanposuru",
+    display: "さんぽする",
+  },
+  {
+    question: "to close (something)",
+    answer: "shimeru",
+    display: "しめる",
+  },
+  {
+    question: "to take out, to let out",
+    answer: "dasu",
+    display: "だす",
+  },
+  {
+    question: "to go out",
+    answer: "dekakeru",
+    display: "でかける",
+  },
+  {
+    question: "to help, to lend a hand",
+    answer: "tetsudau",
+    display: "てつだう",
+  },
+  {
+    question: "to learn (with someone)",
+    answer: "narau",
+    display: "ならう",
+  },
+  {
+    question: "to receive",
+    answer: "morau",
+    display: "もらう",
+  },
+  {
+    question: "to forget",
+    answer: "wasureru",
+    display: "わすれる",
+  },
+  {
+    question: "to give (to me)",
+    answer: "kureru",
+    display: "くれる",
+  },
+  {
+    question: "to take (shower, sunbath)",
+    answer: "abiru",
+    display: "あびる",
+  },
+  {
+    question: "to guide",
+    answer: "annaisuro",
+    display: "あんないする",
+  },
+  {
+    question: "to hurry up",
+    answer: "isogu",
+    display: "いそぐ",
+  },
+  {
+    question: "to drive",
+    answer: "untensuru",
+    display: "うんてんする",
+  },
+  {
+    question: "to push, to press",
+    answer: "osu",
+    display: "おす",
+  },
+  {
+    question: "to remember",
+    answer: "omoidasu",
+    display: "おもいだす",
+  },
+  {
+    question: "to finish",
+    answer: "owaru",
+    display: "おわる",
+  },
+  {
+    question: "to cose, to take (time)",
+    answer: "kakaru",
+    display: "かかる",
+  },
+  {
+    question: "to marry",
+    answer: "ketsukonsuru",
+    display: "けつこんする",
+  },
+  {
+    question: "to worry",
+    answer: "shinpaisuru",
+    display: "しんぱいする",
+  },
+  {
+    question: "to throw away, to abandon",
+    answer: "suteru",
+    display: "すてる",
+  },
+  {
+    question: "to sit down",
+    answer: "suwaru",
+    display: "すわる",
+  },
+  {
+    question: "to explain",
+    answer: "setsumeisuru",
+    display: "せつめいする",
+  },
+  {
+    question: "to stand up",
+    answer: "tatsu",
+    display: "たつ",
+  },
+  {
+    question: "to get tired",
+    answer: "tsukareru",
+    display: "つかれる",
+  },
+  {
+    question: "to be able, can",
+    answer: "dekiru",
+    display: "できる",
+  },
+  {
+    question: "to call on the telephone",
+    answer: "denwawokakeru",
+    display: "でんわをかける",
+  },
+  {
+    question: "to  lose",
+    answer: "nakusu",
+    display: "なくす",
+  },
+  {
+    question: "to take off",
+    answer: "nugu",
+    display: "ぬぐ",
+  },
+  {
+    question: "to climb",
+    answer: "noboru",
+    display: "のぼる",
+  },
+  {
+    question: "to pull",
+    answer: "hiku",
+    display: "ひく",
+  },
+  {
+    question: "to call",
+    answer: "yobu",
+    display: "よぼ",
+  },
+  {
+    question: "to give back, to return",
+    answer: "kaesu",
+    display: "かえす",
+  },
+  {
+    question: "to show",
+    answer: "miseru",
+    display: "みせる",
+  },
+  {
+    question: "to need",
+    answer: "iru",
+    display: "いる",
+  },
+  {
+    question: "to prepare",
+    answer: "jyunbisuru",
+    display: "じゅんびする",
+  },
+  {
+    question: "to win",
+    answer: "katsu",
+    display: "かつ",
+  },
+  {
+    question: "to endeavor, to do one's best",
+    answer: "ganbaru",
+    display: "がんばる",
+  },
+  {
+    question: "to be able to",
+    answer: "dekiru",
+    display: "できる",
+  },
+  {
+    question: "to lose (a game)",
+    answer: "ma-ke-ru",
+    display: "まける",
+  },
+  {
+    question: "to start",
+    answer: "hajimeru",
+    display: "はじめる",
+  },
+  {
+    question: "to change",
+    answer: "kaeru",
+    display: "かえる",
+  },
+  {
+    question: "to be enough",
+    answer: "tariru",
+    display: "たりる",
+  },
+  {
+    question: "to arrive",
+    answer: "tsuku",
+    display: "つく",
+  },
+  {
+    question: "to take (someone)",
+    answer: "tsureteiku",
+    display: "つれていく",
+  },
+  {
+    question: "to bring (someone)",
+    answer: "tsuretekuru",
+    display: "つれてくる",
+  },
+  {
+    question: "to stop",
+    answer: "tomeru",
+    display: "とめる",
+  },
+  {
+    question: "to become",
+    answer: "naru",
+    display: "なる",
+  },
+  {
+    question: "to start, to begin",
+    answer: "hajimaru",
+    display: "はじまる",
+  },
+  {
+    question: "to play (piano, guitar)",
+    answer: "hiku",
+    display: "ひく",
+  },
+  {
+    question: "to fall (rain, snow)",
+    answer: "furu",
+    display: "ふる",
+  },
+  {
+    question: "to carry",
+    answer: "motsuteiku",
+    display: "もつていく",
+  },
+];
+
+export default questions;
